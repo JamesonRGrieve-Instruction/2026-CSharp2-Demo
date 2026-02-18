6839f201533b0092503542227d7e0aa1414e02b6
James Grieve <jamesonrgrieve@gmail.com>
2026-02-17 19:10:56 -0700

    Basic OOP;

---
+++ src/DemoProject/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DemoProject;
+
+
+class ExampleClass
+{
+    int exampleField;
+
+    int ExampleProperty { get; set; }
+
+    int _exampleBackingField;
+    int ExampleFullyImplementedProperty
+    {
+        get
+        {
+            return _exampleBackingField;
+        }
+        set
+        {
+            _exampleBackingField = value;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExampleClass newObject = new ExampleClass();
+        Console.WriteLine("Hello, World!");
+    }
+}
