b53678f80b7e994d509abca98582388d7c78c983
James Grieve <jamesonrgrieve@gmail.com>
2026-03-17 19:06:49 -0600

    Scaffold context and models;

---
+++ src/DemoProject/Models/ExampleContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProject.Models;
+
+public partial class ExampleContext : DbContext
+{
+    public ExampleContext()
+    {
+    }
+
+    public ExampleContext(DbContextOptions<ExampleContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<EfmigrationsLock> EfmigrationsLocks { get; set; }
+
+    public virtual DbSet<ExampleParent> ExampleParents { get; set; }
+
+    public virtual DbSet<ExampleTable> ExampleTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=example.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EfmigrationsLock>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<ExampleTable>(entity =>
+        {
+            entity.HasOne(d => d.Parent).WithMany(p => p.ExampleTables).OnDelete(DeleteBehavior.Restrict);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ src/DemoProject/Models/ExampleParent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProject.Models;
+
+[Table("example_parent")]
+public partial class ExampleParent
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [InverseProperty("Parent")]
+    public virtual ICollection<ExampleTable> ExampleTables { get; set; } = new List<ExampleTable>();
+}
